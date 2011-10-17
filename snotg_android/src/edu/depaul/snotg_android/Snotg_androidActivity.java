951880982db70fb064af8030baa9c6ad3b8bc4ce
Raymond Elward <rayelward@gmail.com>
2011-10-17 18:34:38 -0500

    intial commit

---
+++ snotg_android/src/edu/depaul/snotg_android/Snotg_androidActivity.java
@@ -0,0 +1,13 @@
+package edu.depaul.snotg_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Snotg_androidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
