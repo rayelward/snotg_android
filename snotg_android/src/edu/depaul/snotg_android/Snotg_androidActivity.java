315d396e4c4fda55f069579d0bc52451c0795efd
Raymond Elward <rayelward@gmail.com>
2011-10-17 19:19:27 -0500

    Played around with xml views a bit more.  Created a blank one for profile viewer.

--- snotg_android/src/edu/depaul/snotg_android/Snotg_androidActivity.java
+++ snotg_android/src/edu/depaul/snotg_android/Snotg_androidActivity.java
@@ -8,6 +8,7 @@ public class Snotg_androidActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
+        
     }
 }
