24dc41fda579079f3f5cc540b33cd1cb88771d19
Michael Michalak <mmich8266@gmail.com>
2011-10-27 22:03:34 -0500

    Implemented json read from UserService in MyProfileActivity.java

--- snotg_android/src/edu/depaul/snotg_android/MyProfileActivity.java
+++ snotg_android/src/edu/depaul/snotg_android/MyProfileActivity.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -11,6 +12,9 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
@@ -52,7 +56,7 @@ public class MyProfileActivity  extends Activity  implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		Context context = getApplicationContext();
-		Log.i(INPUT_METHOD_SERVICE, "Michalak Button clicked");
+		Log.i("MyProfileActivity", "Michalak Button clicked");
 		//Toast.makeText(this, "Btn Clicked.  Looking up data....",Toast.LENGTH_SHORT).show();
 		 
 		/*LinearLayout ll = new LinearLayout(this);
@@ -62,15 +66,26 @@ public class MyProfileActivity  extends Activity  implements OnClickListener {
         //Long id = Long.parseLong(intentIn.getStringExtra("notesid")); to get a param
         String user = intentIn.getStringExtra("name");
         //TODO this url must be externalized in order to migrate environments
-        String url = "http://snotgdepaul1.appspot.com/user";
+        String url = "http://10.0.2.2:8888/user";
         String profile = getData(user, url);
-        /*tv.setText(answer);
-        ll.addView(tv);
-        ll.setOrientation(LinearLayout.VERTICAL);
-        setContentView(ll);*/
+ 
         TextView t=(TextView)findViewById(R.id.GAE_ping_return); 
-        Log.i("GAE", profile);
-        t.setText(profile);
+        Log.d("MyProfileActivity", profile);
+        HashMap<String, String> ret = readJsonReturnObj(profile);
+        t.setText(ret.get("msg").toString() + ret.get("ts").toString());
+	}
+	
+	public void writeJson() {
+		JSONObject object = new JSONObject();
+		try {
+			object.put("name", "Jack Hack");
+			object.put("score", new Integer(200));
+			object.put("current", new Double(152.32));
+			object.put("nickname", "Hacker");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		Log.i("JSON", object.toString());
 	}
 	
     public String getData(String user, String url)
@@ -91,15 +106,49 @@ public class MyProfileActivity  extends Activity  implements OnClickListener {
 			while ((line = br.readLine()) != null)
 				result.append(line);
 		} 
-		catch (ClientProtocolException e) 
-		{
+		catch (ClientProtocolException e) {
 			e.printStackTrace();
 		}
-		catch (IOException e) 
-		{
+		catch (IOException e) {
 			e.printStackTrace();
-		}//catch
+		}
 		
     	return result.toString();
     }
+    
+    public HashMap readJsonReturnArray(String jsonReturn)
+    {
+    	HashMap<String, String> ret = new HashMap<String, String>();
+    	try {
+			JSONArray jsonArray = new JSONArray(jsonReturn);
+			Log.i(MyProfileActivity.class.getName(),
+					"Number of entries " + jsonArray.length());
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject jsonObject = jsonArray.getJSONObject(i);
+				ret.put("msg", (String)jsonObject.get("msg"));
+				ret.put("ts", (String)jsonObject.get("ts"));
+				//Log.i(MyProfileActivity.class.getName(), jsonObject.getString("date"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}   
+		return ret;
+	}
+    
+    public HashMap readJsonReturnObj(String jsonReturn)
+    {
+    	HashMap<String, String> ret = new HashMap<String, String>();
+    	try {
+    		JSONObject jsonObj = new JSONObject(jsonReturn);
+			Log.i(MyProfileActivity.class.getName(), 
+					"Number of entries " + jsonObj.length());
+			ret.put("msg", (String)jsonObj.get("msg"));
+			ret.put("ts", (String)jsonObj.get("ts"));
+			//Log.i(MyProfileActivity.class.getName(), jsonObject.getString("date"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}   
+		return ret;
+	}
+
 }
