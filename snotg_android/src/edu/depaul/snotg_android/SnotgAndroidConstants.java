daf09c7dc79f5fbf5dc66b40b9dbdf78abaca29d
Raymond Elward <rayelward@gmail.com>
2011-11-19 16:29:33 -0600

    change constants to new GAE location.  minute heartbeat count.

--- snotg_android/src/edu/depaul/snotg_android/SnotgAndroidConstants.java
+++ snotg_android/src/edu/depaul/snotg_android/SnotgAndroidConstants.java
@@ -2,10 +2,10 @@ package edu.depaul.snotg_android;
 
 public class SnotgAndroidConstants {
 
-	public static final long HEARTBEAT_INTERVAL = 30 * 1000; // sec * 1000
+	public static final long HEARTBEAT_INTERVAL = 60 * 1000; // sec * 1000
 	
 	// These are used to create an apache http client request.  See JsonUtility
-	public static final String URI_BACKEND_HOSTNAME = "se491snotg.appspot.com";
+	public static final String URI_BACKEND_HOSTNAME = "se491snotg-1.appspot.com";
 	public static final int URI_BACKEND_PORT = 80;
 	public static final String URI_PROTOCOL = "http";
 	public static final String URI_PATH_HEARTBEAT = "/user_locations";
